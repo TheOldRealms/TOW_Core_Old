56d5900400b9bbae3affe612476ee776f473d267
tristanka <tristanarm@gmail.com>
2021-05-01 18:01:04 -0500

    Add the actual harmony patch

---
+++ HarmonyPatches/DecideAgentHitParticlesPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade;
+using TOW_Core.Battle.Extensions;
+
+namespace TOW_Core.HarmonyPatches
+{
+    [HarmonyPatch]
+    public static class DecideAgentHitParticlesPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Mission), "DecideAgentHitParticles")]
+        public static bool Prefix(Agent victim)
+        {
+            if(victim != null && victim.IsUndead())
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
