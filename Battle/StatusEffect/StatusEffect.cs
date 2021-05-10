429e1401a9860bce3251aebb8d50240a099faf89
Linus <linus-tiemann@live.de>
2021-05-11 01:56:38 +0200

    idea of effectContainer
    this is the idea of updating current status effects with a merged effect container

--- Battle/StatusEffect/StatusEffect.cs
+++ Battle/StatusEffect/StatusEffect.cs
@@ -1,4 +1,6 @@
-﻿using TaleWorlds.MountAndBlade;
+﻿using System.Collections.Generic;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
 
 namespace TOW_Core.Battle.StatusEffect
 {
@@ -10,7 +12,44 @@ namespace TOW_Core.Battle.StatusEffect
         public float duration { get; set; }
         public float _currentduration;
 
-        public Agent affector = null; 
+        public Agent affector = null;
+
+        public EffectType _EffectType;
+
+
+        public EffectContainer EffectContainer;
+        public enum EffectType
+        {
+            Armor, 
+            Health, 
+            Damage
+            EOT
+        }
+
+
+        public void Activate()
+        {
+            
+        }
+
         //needs refinement since this can't be defined. and need to be assigned on runtime
     }
+    
+    
+    public class EffectContainer
+    {
+        public float _healthOverTime;  //all hots(heal over time) and dots(damge over time)  negative numbers resemble a dot.
+        public List<DamageTypes> receivedDamageTypes;
+        
+        //armor related
+        public float WardSaveFactor;  // between 0 and 1 , 1 means full damage, 0 means 0 damage
+        public float Armorvalue;      //between 0 and infite added at the end of damage calculation of beeing hit
+        public float ArmorPercentage; //percental 
+        private List<DamageTypes> protectionTypes;// all Damage types the agent suffers of.
+        //damage related
+        public float _damageValue;      //between 0 and infinite added at the end of a damage calculation of a hit. base Damage
+        public float _damagePercentage; //damage Percentage after all effects are added
+        public List<DamageTypes> _attackDamageTypes;   //NOT FUNCTIONAL Implies all kind of damage types for an output attack
+    }
+}
 }
--- Battle/StatusEffect/StatusEffectComponent.cs
+++ Battle/StatusEffect/StatusEffectComponent.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 using TaleWorlds.Core;
+using TaleWorlds.InputSystem;
 using TaleWorlds.MountAndBlade;
 
 namespace TOW_Core.Battle.StatusEffect
@@ -18,21 +19,25 @@ namespace TOW_Core.Battle.StatusEffect
         private Dictionary<int,StatusEffect> _statusEffects;
         private List<int> _currentEffects;
         
-        
+        private float _baseHealth; //to be assigned by the agent, will not be changed during mission
+        private float _currentHealth;
         //TODO refine the active status values
-        //health and Hots, Dots
-        private float _baseHealth;    //to be assigned by the agent, will not be changed during mission
+        
+            
         private float _bonusHealth;     //additional health on top of the base health. 
-        private int _currentHealth;
+        private float _healthPercentage;
+      
         private float _healthOverTime;  //all hots(heal over time) and dots(damge over time)  negative numbers resemble a dot.
         private List<DamageTypes> receivedDamageTypes; // all Damage types the agent suffers of.
         //armor related
         private float _WardSaveFactor;  // between 0 and 1 , 1 means full damage, 0 means 0 damage
         private float _armorvalue;      //between 0 and infite added at the end of damage calculation of beeing hit
+        private float _armorPercentage; //percental 
         private List<DamageTypes> protectionTypes;
         
         //damage output
         private float _damageValue;      //between 0 and infinite added at the end of a damage calculation of a hit. base Damage
+        private float _damagePercentage; //damage Percentage after all effects are added
         private List<DamageTypes> _attackDamageTypes;   //NOT FUNCTIONAL Implies all kind of damage types for an output attack
         
         
@@ -61,35 +66,60 @@ namespace TOW_Core.Battle.StatusEffect
             else
             {
                 _currentEffects.Add(effect.id);
+                UpdateEffects();
             }
             
         }
         
         protected override void  OnTickAsAI(float dt)
         {
+           
+        }
+        public void OnTick(object sender, OnTickArgs args)
+        {
+            _currentHealth += _healthOverTime * args.deltatime; //TODO maybe check this somewhere different, but might make sense also here
+            
             if(!_currentEffects.IsEmpty())
             {
                 foreach (var key in _currentEffects)
                 {
-                    _statusEffects[key]._currentduration = _currentEffects[key] - dt;
+                    _statusEffects[key]._currentduration = _currentEffects[key] - args.deltatime;
 
                     if (_statusEffects[key]._currentduration<= 0f)
                     {
                         _statusEffects[key]._currentduration = _statusEffects[key].duration;
                         _currentEffects.Remove(key);
+                        UpdateEffects();
                     }
                 }
             }
         }
-        public void OnTick(object sender, OnTickArgs args)
+
+
+        private void UpdateEffects()
         {
-           
+            EffectContainer MergeContainer = new EffectContainer();
+            foreach (var key in _statusEffects)
+            {
                 
-        }
+                var container = key.Value.EffectContainer;
+                switch (key.Value._EffectType)
+                {
+                    case StatusEffect.EffectType.Armor:
+                        MergeContainer.Armorvalue+= container.Armorvalue;
+                        MergeContainer.ArmorPercentage += container.ArmorPercentage;
+                        MergeContainer.WardSaveFactor += container.WardSaveFactor;
+                        break;
+                    //... Health ... Damage
+                }
+            }
+
+            _armorvalue = MergeContainer.Armorvalue;
+            _armorPercentage = MergeContainer.ArmorPercentage;
+            _WardSaveFactor = MergeContainer.WardSaveFactor;
+            //... other effects 
 
 
-        public void UpdateEffects()
-        {
             //TODO this is done once an status effect is added, or removed, it recalculates the fields which are included in the Damage Model
         }
     }
