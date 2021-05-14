288b3c1f946bb1188d57b73fb35f0df9652956c0
jgajos <finnpeert@gmail.com>
2021-05-14 21:55:25 +0200

    Add Initial screen button.

---
+++ Screen/InitialScreen/TOWInitialGauntletScreen.cs
@@ -0,0 +1,31 @@
+﻿
+using TaleWorlds.Core;
+using TaleWorlds.Engine.GauntletUI;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.GauntletUI;
+using TaleWorlds.MountAndBlade.View.Screen;
+
+using TOW_Core.Screen.InitialScreen;
+
+namespace TOW_Core.Screen
+{
+    [GameStateScreen(typeof(InitialState))]
+    public class TOWInitialGauntletScreen : InitialGauntletScreen
+    {
+        private GauntletLayer _gauntletLayer;
+        private TOWInitialScreenVM _dataSource;
+
+        public TOWInitialGauntletScreen(InitialState state) : base(state)
+        {
+
+            this._dataSource = new TOWInitialScreenVM("BUTTON TEXT", () => InformationManager.DisplayMessage(new InformationMessage("Hello World!")));
+            this._gauntletLayer = new GauntletLayer(3, "GauntletLayer");
+            this._gauntletLayer.LoadMovie("TOWInitialScreenOverlay", this._dataSource);
+
+            this._gauntletLayer.InputRestrictions.SetInputRestrictions(true, InputUsageMask.Mouse);
+
+            base.AddLayer(this._gauntletLayer);
+        }
+    }
+}
---
+++ Screen/InitialScreen/TOWInitialScreenVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using TaleWorlds.Library;
+
+namespace TOW_Core.Screen.InitialScreen
+{
+    public class TOWInitialScreenVM : ViewModel
+    {
+        private Action action;
+        private string text;
+
+        public TOWInitialScreenVM(string text, Action action)
+        {
+            this.text = text;
+            this.action = action;
+        }
+
+        [DataSourceProperty]
+        public string NameText
+        {
+            get
+            {
+                return this.text;
+            }
+        }
+
+        public void ExecuteAction()
+        {
+            this.action();
+        }
+    }
+}
