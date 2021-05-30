5d4fc9ea50fdcdd3586b723315b4782caeb03acc
Linus <linus-tiemann@live.de>
2021-05-30 15:31:26 +0200

    test for initializing own manager

---
+++ CampaignMode/Tow_CampaignManager.cs
@@ -0,0 +1,30 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TOW_Core.Utilities;
+
+namespace TOW_Core.CampaignMode
+{
+    
+    public  class Tow_CampaignManager
+    {
+        public static readonly Tow_CampaignManager instance;
+        
+        private Tow_CampaignManager(){}
+
+        static Tow_CampaignManager()
+        {
+            instance = new Tow_CampaignManager();
+        }
+        public static Tow_CampaignManager Instance => instance;
+
+
+
+        public void Hello()
+        {
+            Utilities.TOWCommon.Say("Hello");
+        }
+        
+        
+        
+    }
+}
---
+++ HarmonyPatches/OnCampaignMapInitalizer.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TOW_Core.CampaignMode;
+
+namespace TOW_Core.HarmonyPatches
+{
+    [HarmonyPatch(typeof(CampaignObjectManager))]
+    public static class OnCampaignMapInitalizerPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("OnLoad")]
+        public static void PostFix()
+        {
+            Tow_CampaignManager.Instance.Hello();
+        }
+    }
+}
+
+
+namespace TOW_Core.HarmonyPatches
+{
+    
+    }
